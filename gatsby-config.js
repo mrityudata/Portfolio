85b03c8329f3793f865bf13a3689ce94036ac0b6
Amit <amitthakursahanpur@gmail.com>
2022-03-14 11:53:57 +0530

    The Poetfolio

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
     siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `i-Amit_Chauhan`,
     siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
     siteUrl: `https://cara.lekoarts.de`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
@@ -33,7 +33,7 @@ module.exports = {
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#f6ad55`,
-        display: `standalone`,
+        display: `standalone`, 
         icons: [
           {
             src: `/android-chrome-192x192.png`,
@@ -44,7 +44,7 @@ module.exports = {
             src: `/android-chrome-512x512.png`,
             sizes: `512x512`,
             type: `image/png`,
-          },
+          }
         ],
       },
     },
